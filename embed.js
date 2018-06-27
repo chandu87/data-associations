f28719b6dede13d4bf9ddd31cecf265aebe3caf2
chandu <chandrasr.palle@gmail.com>
2018-06-27 13:47:55 +0200

    reading the data from db and adding more content"

--- embed.js
+++ embed.js
@@ -16,22 +16,22 @@ const User = mongoose.model("User", userSchema);
 
 
 
-const newUser = new User({
-    name: "Mahesh Babu",
-    email: "charlie@brown.edu"
-});
-newUser.posts.push({
-    title:"Srimanthudu",
-    content: "Blocbuster - Must watch movie"
-});
+// const newUser = new User({
+//     name: "Mahesh Babu",
+//     email: "charlie@brown.edu"
+// });
+// newUser.posts.push({
+//     title:"Srimanthudu",
+//     content: "Blocbuster - Must watch movie"
+// });
 
-newUser.save(function(err, data){
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-});
+// newUser.save(function(err, data){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(data);
+//     }
+// });
 // const newPost = new Post({
 //     title: "Apples",
 //     content: "There so tasty"
@@ -44,3 +44,21 @@ newUser.save(function(err, data){
 //         console.log(post);
 //     }
 // });
+User.findOne({name: "Mahesh Babu"}, function(err, user){
+    if(err)
+    {
+        console.log(err);
+    }else{
+        user.posts.push({
+            title : "Athadu",
+            content: "Family thriller 200Days movie"
+        });
+        user.save(function(err, userData){
+            if(err){
+                console.log(err);
+            }else{
+                console.log(userData);
+            }
+        });
+    }
+})
